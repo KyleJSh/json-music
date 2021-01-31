473f9f26348492e319aa2c61acd63606c2af4008
Kyle Sherrington <kylejsherrington@gmail.com>
2021-01-31 12:58:31 -0800

    Initial Commit

---
+++ json-music/json_musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  json_musicApp.swift
+//  json-music
+//
+//  Created by Kyle Sherrington on 2021-01-31.
+//
+
+import SwiftUI
+
+@main
+struct json_musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
